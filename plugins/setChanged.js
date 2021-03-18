2904bf9a91bea6c3c984a7cee5e02e511ac5d3bd
juvian <juliandelgobbo@gmail.com>
2021-03-18 00:57:20 -0300

    change dependency to repo

--- plugins/setChanged.js
+++ plugins/setChanged.js
@@ -2,11 +2,10 @@ const {mapIterator} = require('../utils/utils');
 
 const afterParse = (output) => {
     for (const {id, data} of mapIterator(output)) {
-        if (data.hasOwnProperty("newUntranslated") && data.hasOwnProperty("oldUntranslated") && data.newUntranslated != data.oldUntranslated) {
+        //new string (or old unmatched from string)
+        if ((data.hasOwnProperty("newUntranslated") && data.hasOwnProperty("oldUntranslated") && data.newUntranslated != data.oldUntranslated) || !data.oldTranslated) { 
             data.changed = true;
             data.newTranslated = data.newTranslated || "";
-        } else if(!data.oldTranslated) { //new string (or old unmatched from string)
-            data.changed = true;
         }
     }
 }
