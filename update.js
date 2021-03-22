00b01d5991f45cb6c9a5c2244dbacd5ed3c732c2
juvian <juliandelgobbo@gmail.com>
2021-03-22 00:42:37 -0300

    fix update

--- update.js
+++ update.js
@@ -3,13 +3,13 @@ const path = require('path');
 const {deserialize, serialize} = require('./utils/utils');
 const {parseArgs} = require('./utils/argParser');
 
-const args = await parseArgs(process.argv.slice(2));
-const plugins = args.filter(a => a.type == 'plugin');
-const inputLocation = args.find(a => a.type == 'output');
-
-const input = deserialize(path.extname(inputLocation), fs.readFileSync(inputLocation));
-
 async function main() {
+    const args = await parseArgs(process.argv.slice(2));
+    const plugins = args.filter(a => a.type == 'plugin');
+    const inputLocation = args.find(a => a.type == 'output').arg;
+
+    const input = deserialize(path.extname(inputLocation), fs.readFileSync(inputLocation));
+
     for (const plugin of plugins) {
         if (plugin.module.afterParse) await plugin.module.afterParse(input);
     }
