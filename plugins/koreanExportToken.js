31852d5769930a32887bba840fb67103e07a77da
juvian <juliandelgobbo@gmail.com>
2021-03-23 20:52:31 -0300

    handle update cases where there is oldTranslated

--- plugins/koreanExportToken.js
+++ plugins/koreanExportToken.js
@@ -1,6 +1,6 @@
 const {koreanRegex} = require('../utils/tokenizer');
 
 const shouldTranslateString = (str) => str.match(koreanRegex);
-const shouldTranslateData = (data) => data.untranslated;
+const shouldTranslateData = (data) => !data.newTranslated && (data.untranslated || data.changed);
 
 module.exports = {shouldTranslateString, shouldTranslateData}
